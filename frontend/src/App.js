172905d96933600ca5025bb14c264178102c519b
Musstah <pythonapilearn@gmail.com>
2024-02-16 14:45:41 +0100

    Started with profile, need to pass token

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/recipes/:recipeId" element={<Recipe />} />
             <Route path="/recipes" element={<Recipes />} />
-            <Route path="/profile" element={<SignIn />} />
+            <Route path="/profile" element={<Profile />} />
             <Route path="/sign-in" element={<SignIn />} />
             <Route path="/sign-up" element={<SignUp />} />
             <Route path="/forgot-password" element={<SignIn />} />
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 function Profile() {
-  return <div>Profile</div>;
+  const [currentUser, setcurrentUser] = useState(null);
+
+  useEffect(() => {
+    const fetchCurrentUser = async () => {
+      try {
+        const response = await fetch("/api/v1/auth/me", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1OTdmNDI2Y2Q5OTRmYzQ4MmY0ZGQ4ZCIsImlhdCI6MTcwODA5MDQ4NiwiZXhwIjoxNzEwNjgyNDg2fQ.cT22Bb6V1zZeQHeYOE6N-Kz-5yACdcXUyvtB4BHYkW0`,
+          },
+        });
+        const json = await response.json();
+        setcurrentUser(json);
+        console.log(currentUser);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchCurrentUser();
+  }, []);
+
+  return (
+    <>
+      {currentUser ? (
+        <>
+          <p>
+            {`User ID: ${currentUser.data._id}`}
+            <br></br>
+          </p>
+          <p>
+            {`User Name: ${currentUser.data.name}`}
+            <br></br>
+          </p>
+          <p>
+            {`User Email: ${currentUser.data.email}`}
+            <br></br>
+          </p>
+        </>
+      ) : (
+        "No User"
+      )}
+    </>
+  );
 }
 
 export default Profile;
