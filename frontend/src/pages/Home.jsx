1dcb2c4b679c9e4403cfa92b6c5c272c7064d6fb
Musstah <pythonapilearn@gmail.com>
2024-03-02 23:40:20 +0100

    Need to make component for tags

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -11,7 +11,7 @@ function Home() {
 
   const getRandomArray = (max) => {
     const randomArr = [];
-    while (randomArr.length < 3) {
+    while (randomArr.length < 8) {
       const tempVal = Math.floor(Math.random() * max);
       if (!randomArr.includes(tempVal)) {
         randomArr.push(tempVal);
@@ -32,12 +32,29 @@ function Home() {
       <h1 className="mb-4 text-4xl font-bold my-6 text-cyan-700 text-center">
         Dom's Cullinary Craft
       </h1>
-      <div className="flex flex-row h-32 bg-red-400 mt-12 mb-12"></div>
-      <div className="flex flex-row mt-40">
+      <div className="flex overflow-x-auto whitespace-nowrap space-x-2">
+        {/* Tag Item 1 */}
+        <div
+          className="relative flex flex-col justify-end flex-grow-0 flex-shrink-0 flex-basis-0 w-24 h-24 rounded-2xl 
+        bg-gradient-to-br from-stone-50 via-stone-200 to-stone-100 my-6 mx-2 shadow-lg
+        text-center"
+        >
+          <img
+            src="/pizzaTag.png"
+            alt="img"
+            className="absolute md:hidden object-cover rotate-[200deg] rounded-xl w-4/6 h-4/6 -right-1 -top-1"
+          />
+          <strong className="self-bottom pb-2">Pizza</strong>
+        </div>
+      </div>
+
+      {/* ------------------------------------------------------- */}
+
+      <div className="flex overflow-x-auto whitespace-nowrap">
         {randomArr.map((val, index) => (
           <div key={index}>
             {data.data[val] ? (
-              <div className="group relative overflow-hidden px-2 w-1/2 mb-4 md:px-8">
+              <div className="group relative overflow-hidden px-2 w-60 mb-4 md:px-8">
                 <Link
                   to={`recipes/${data.data[val]._id}`}
                   key={index}
@@ -54,19 +71,10 @@ function Home() {
                   <img
                     src={`/${data.data[val].img}`}
                     alt="img"
-                    className="md:hidden w-40 h-40 rounded-xl object-cover"
+                    className="md:hidden object-cover h-80 rounded-xl"
                   />
                   {/* Info Container */}
                   <div className="flex flex-row w-11/12 mt-2 mb-4 justify-between">
-                    <div
-                      className="flex flex-row items-center space-x-2 bg-cyan-400 px-3 
-            py-0.5 rounded-xl bg-opacity-70"
-                    >
-                      <FontAwesomeIcon icon={faStar} />
-                      <p className="font-bold">
-                        {data.data[val].averageRating}
-                      </p>
-                    </div>
                     <strong className="pr-3">{`${data.data[val].averageTime} mins`}</strong>
                   </div>
 
