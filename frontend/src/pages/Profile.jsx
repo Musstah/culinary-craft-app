4286dd78292a232e882f67fe6e0e4231bfb622f0
Musstah <pythonapilearn@gmail.com>
2025-07-20 20:20:27 +0200

    Dodano profiel view. Up to mobile design then!

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -30,50 +30,65 @@ function Profile() {
       {currentUser ? (
         // Container Div
         <div className="flex flex-col items-center mx-2">
-          <div
-            className="flex flex-row justify-between items-center w-full md:my-10 md:ml-24 md:relative
-          "
-          >
-            <h2 className="mb-4 text-4xl font-bold my-24 text-gray-900 text-center md:my-0">
+          <div className="flex flex-row items-center justify-between w-full md:my-10 md:ml-24 md:relative ">
+            <h2 className="font-serif text-5xl font-bold tracking-wide text-stone-600 dark:text-stone-300">
               My Profile
             </h2>
-            <button
-              className="md:absolute md:right-12 md:top-16 h-10 text-white bg-gradient-to-r from-cyan-400 via-cyan-500 
-                to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 
-                dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 
-                font-medium rounded-lg text-sm px-5 mt-20 text-center md:mr-12"
-              onClick={logOut}
-            >
-              Logout
-            </button>
           </div>
 
           {/* Horizontal Line */}
-          <div className="w-11/12 border-b border-zinc-500 opacity-50 mt-4 mb-2 md:mt-12 md:mb-14"></div>
-          <div className="flex flex-col items-center w-full md:w-1/4">
-            <div className="flex flex-row w-full md:w-11/12 justify-between items-center my-2">
-              <p className="font-thin text-black text-left pl-2">
-                Personal Details
-              </p>
-              <Link to="#" className="font-thin text-sky-500 opacity-90 pr-5">
-                Change
-              </Link>
-            </div>
-            <div className="flex flex-col justify-center px-4 py-2 h-20 w-11/12 rounded-2xl bg-slate-50">
-              <p className="py-1 font-bold">{currentUser.data.name}</p>
-              <p className="font-bold">{currentUser.data.email}</p>
-            </div>
-            <div className="flex flex-row mx-auto justify-center w-full md:w-96 mt-12">
+          <div className="w-11/12 mt-4 border-b opacity-50 border-zinc-500 md:mt-12 md:mb-14"></div>
+          <div className="flex flex-col items-center w-full mt-8 space-y-6 text-center border border-gray-300 md:w-1/4 bg-slate-50 dark:bg-neutral-950 dark:border-gray-700">
+            <img
+              className="object-cover w-48 h-48 mt-6 rounded-full"
+              src={currentUser.data.image || "/avatar.jpg"}
+              alt=""
+            />
+
+            <p className="mt-2 font-serif text-3xl font-bold tracking-wide md:mt-6 md:text-5xl text-stone-600 dark:text-stone-300">
+              {currentUser.data.name}
+            </p>
+            <p className="font-serif text-xl font-bold tracking-wide md:text-2xl text-stone-600 dark:text-stone-300">
+              {currentUser.data.email}
+            </p>
+
+            <div className="flex flex-col items-center w-5/6 space-y-6">
               <Link
                 to="#"
-                className="flex items-center w-5/6 text-white bg-gradient-to-r from-teal-400 via-teal-500
-                 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300
-                  dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 
-                  font-medium rounded-lg text-sm px-2 py-3 text-center me-2 mb-8"
+                className="w-5/6 px-5 py-3 text-sm font-medium bg-white border border-gray-300 dark:border-0
+                hover:bg-gray-100  focus:ring-gray-100 text-center dark:text-white 
+          dark:bg-[#1f1f1f] hover:bg-[#24292F]/90
+            focus:ring-4 focus:outline-none 
+          focus:ring-[#2a2a2a]/50 rounded-lg  
+          dark:focus:ring-[#2a2a2a]/50 dark:hover:bg-[#2a2a2a]/30 hover:-translate-y-0.5"
               >
-                <FontAwesomeIcon icon={faBowlFood} className="w-5 h-5" />
-                <span className="flex-1 pr-4">Add a New Recipe</span>
+                Edit Profile
               </Link>
+              <Link
+                to="#"
+                className=" w-5/6 px-5 py-3 text-sm font-medium bg-white border border-gray-300 dark:border-0
+                hover:bg-gray-100  focus:ring-gray-100 text-center dark:text-white 
+          dark:bg-[#1f1f1f] hover:bg-[#24292F]/90
+            focus:ring-4 focus:outline-none 
+          focus:ring-[#2a2a2a]/50 rounded-lg  
+          dark:focus:ring-[#2a2a2a]/50 dark:hover:bg-[#2a2a2a]/30 hover:-translate-y-0.5"
+              >
+                Add New Recipe
+              </Link>
+              <button
+                onClick={logOut}
+                to="/sign-up"
+                className="w-5/6 px-5 py-3  text-sm font-medium bg-white border border-gray-300 dark:border-0
+                hover:bg-gray-100  focus:ring-gray-100 text-center dark:text-white 
+          dark:bg-[#1f1f1f] hover:bg-[#24292F]/90
+            focus:ring-4 focus:outline-none 
+          focus:ring-[#2a2a2a]/50 rounded-lg  
+          dark:focus:ring-[#2a2a2a]/50 dark:hover:bg-[#2a2a2a]/30 hover:-translate-y-0.5"
+              >
+                Logout
+              </button>
+              {/* This div is to add margin on bottom */}
+              <div className="h-2"></div>
             </div>
           </div>
         </div>
